c5d333463f480e10533500e722cee5685644940d
黄俊达 <jdhuang@ust.hk>
2016-10-14 14:15:35 +0800

    初始化项目

---
+++ MyRepositoryTest001/MyRepositoryTest001/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyRepositoryTest001
+//
+//  Created by Jorton on 16/10/14.
+//  Copyright © 2016年 abchtv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyRepositoryTest001/MyRepositoryTest001/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyRepositoryTest001
+//
+//  Created by Jorton on 16/10/14.
+//  Copyright © 2016年 abchtv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
